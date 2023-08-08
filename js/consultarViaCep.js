a02ede3275d61d716f85edf9f4417fd2c5eccf9c
Vitor Garcia de Vasconcelos <vitorgarciadevasconcelos@gmail.com>
2023-08-08 10:15:54 -0300

    Mudanças no HTML e adicionei arquivo CSS

--- js/consultarViaCep.js
+++ js/consultarViaCep.js
@@ -1,17 +1,26 @@
 /////////   Versão 2: para chamar diretamente do HTML /////////
 //O valor do cep digitado está no <input> com id "txtCep"
 async function buscarCEP() {
-    //'document' é uma variável global que representa todo o HTML e seus elementos (a árvore DOM - Document Object Model)
 
+    limpar();
+    //'document' é uma variável global que representa todo o HTML e seus elementos (a árvore DOM - Document Object Model)
     var txtCep = document.getElementById('txtCep');
     var cepInformado = txtCep.value;
+    
+    // By ID = único, NAME pode se repetir
+    var fieldsetCep = document.getElementsByName('fieldset-consulta-cep');
+    if (cepInformado.length == 8) {
+        fieldsetCep.style = 'background-color: purple ';
+    } else {
+        fieldsetCep.style = 'background-color: orange';
+    }
 
     const promiseConsultaCEP = await fetch(`https://viacep.com.br/ws/${cepInformado}/json/`);
 
     //Retorno da Promise
     const json = await promiseConsultaCEP.json();
 
-    if(json.erro) {
+    if (json.erro) {
         mostrarTelaErro();
     } else {
         preencherCamposComJson();
@@ -19,26 +28,32 @@ async function buscarCEP() {
 
 
     //Preencher os dados do endereço obtido na página HTML
-    //Obter o componente pelo ID , funciona!! 
-    // ESSA condição funciona em javascript, é o mesmo que
-    //if(json.bairro != undefinde && json.bairro != '')
-    if (json.bairro) {
-        txtBairro.value = json.bairro;
-    } else {
-        txtBairro.disabled = false;
-    }
+    function preencherCamposComJson(json) {
+        // ESSA condição funciona em javascript, é o mesmo que
+        //if(json.bairro != undefinde && json.bairro != '')
+        if (json.bairro) {
+            //Obter o componente pelo ID , funciona!! 
+            txtBairro.value = json.bairro;
+        } else {
+            txtBairro.disabled = false;
+        }
 
-    // VERSÃO 2:    // Tem esse dois jeitos de pegar um valor do JSON
-    document.getElementById('txtUF').value = json.uf;
-    txtCidade.value = json.localidade;
+        // VERSÃO 2:    // Tem esse dois jeitos de pegar um valor do JSON
+        document.getElementById('txtUF').value = json.uf;
+        txtCidade.value = json.localidade;
+
+        document.getElementById('txtRua').value = json.rua;
+        txtRua.value = json.logradouro;
+    }
 }
 
 function limpar() {
 
     txtBairro.value = '';
     txtCidade.value = '';
-    txtUF.value = "";
-    
+    txtUF.value = '';
+    txtRua.value = '';
+
     txtBairro.disabled = true;
 }
 
@@ -47,4 +62,8 @@ function mostrarTelaErro() {
     // Complementar
     alert('CEP informado não existe')
 }
-///////////////////////////////////////////////////////////////
+
+function salvarEndereco() {
+    //TODO chamar no back end
+
+}
