eba549b30aedb9a0868eab1ce36831ee0ff52a17
Vitor Garcia de Vasconcelos <vitorgarciadevasconcelos@gmail.com>
2023-08-08 11:15:24 -0300

    Mudanças no estilo CSS e ajustes na consulta

--- js/consultarViaCep.js
+++ js/consultarViaCep.js
@@ -1,54 +1,47 @@
 /////////   Versão 2: para chamar diretamente do HTML /////////
 //O valor do cep digitado está no <input> com id "txtCep"
 async function buscarCEP() {
-
     limpar();
     //'document' é uma variável global que representa todo o HTML e seus elementos (a árvore DOM - Document Object Model)
     var txtCep = document.getElementById('txtCep');
     var cepInformado = txtCep.value;
-    
+
     // By ID = único, NAME pode se repetir
-    var fieldsetCep = document.getElementsByName('fieldset-consulta-cep');
-    if (cepInformado.length == 8) {
-        fieldsetCep.style = 'background-color: purple ';
-    } else {
-        fieldsetCep.style = 'background-color: orange';
-    }
-
-    const promiseConsultaCEP = await fetch(`https://viacep.com.br/ws/${cepInformado}/json/`);
-
-    //Retorno da Promise
-    const json = await promiseConsultaCEP.json();
-
-    if (json.erro) {
+    fetch(`https://viacep.com.br/ws/${cepInformado}/json/`)
+    .then(resultado => resultado.json)
+    .then(json => {
+        if(json.erro){
+            mostrarTelaErro();
+        }else{
+            preencherCamposComJson();
+        }    
+    })
+    .catch(erro => {
         mostrarTelaErro();
+    })      
+}
+
+//Preencher os dados do endereço obtido na página HTML
+function preencherCamposComJson(json) {
+    // ESSA condição funciona em javascript, é o mesmo que
+    //if(json.bairro != undefinde && json.bairro != '')
+    if (json.bairro) {
+        //Obter o componente pelo ID , funciona!! 
+        txtBairro.value = json.bairro;
     } else {
-        preencherCamposComJson();
+        txtBairro.disabled = false;
     }
 
+    // VERSÃO 2:    // Tem esse dois jeitos de pegar um valor do JSON
+    document.getElementById('txtUF').value = json.uf;
+    txtCidade.value = json.localidade;
 
-    //Preencher os dados do endereço obtido na página HTML
-    function preencherCamposComJson(json) {
-        // ESSA condição funciona em javascript, é o mesmo que
-        //if(json.bairro != undefinde && json.bairro != '')
-        if (json.bairro) {
-            //Obter o componente pelo ID , funciona!! 
-            txtBairro.value = json.bairro;
-        } else {
-            txtBairro.disabled = false;
-        }
-
-        // VERSÃO 2:    // Tem esse dois jeitos de pegar um valor do JSON
-        document.getElementById('txtUF').value = json.uf;
-        txtCidade.value = json.localidade;
-
-        document.getElementById('txtRua').value = json.rua;
-        txtRua.value = json.logradouro;
-    }
+    document.getElementById('txtRua').value = json.rua;
+    txtRua.value = json.logradouro;
 }
 
 function limpar() {
-
+    divDadosEndereco.style = 'background-color: blue';
     txtBairro.value = '';
     txtCidade.value = '';
     txtUF.value = '';
@@ -59,11 +52,6 @@ function limpar() {
 
 function mostrarTelaErro() {
     limpar();
-    // Complementar
-    alert('CEP informado não existe')
-}
-
-function salvarEndereco() {
-    //TODO chamar no back end
-
+    divDadosEndereco.style = 'background-color: red';
+    alert('CEP informado não existe');
 }
