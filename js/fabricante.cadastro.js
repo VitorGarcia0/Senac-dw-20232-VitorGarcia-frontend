192a258120831ffe5f0ca33ed377d9b734ec67a6
Vitor Garcia de Vasconcelos <vitorgarciadevasconcelos@gmail.com>
2023-09-21 11:10:47 -0300

    Mudanças no cadastro de fabricante

--- js/fabricante.cadastro.js
+++ js/fabricante.cadastro.js
@@ -1,18 +1,27 @@
-async function CadastrarFabricante() {
+async function cadastrarFabricante() {
    // buscarCEP();
     // fazer um post para cadastrar o Fabricante
     let options = {
         method: "POST",
-        headers: { 'Content-type': "application/json" },
+        headers: { 'Content-type': "application/json; charset=UTF-8" },
         body: JSON.stringify({
-            nome: document.getElementById("nome").value,
+            nome: document.getElementById("nome").value,   //podia passar só nome.value (pegar pelo id do label)
             cnpj: document.getElementById("cnpj").value,
             cep: document.getElementById("cep").value,
             cidade: document.getElementById("cidade").value,
-            uf: document.getElementById("uf").value,
-
+            uf: document.getElementById("uf").value
         })
     }
+    .then(response => {
+        if(response.status == 500){
+            alert('Preencha todos os campos');
+        }
+        response.json();
+    } ) 
+    .then(json => {
+        alert("Fabricante salvo com sucesso");
+        limparCampos();
+    });
    const registroFabricante = await fetch('localhost:8080/api/fabricante', options);
    const registrarFabricante = await registroFabricante.json();
 }
@@ -26,33 +35,34 @@ async function buscarCep() {
     fetch(`https://viacep.com.br/ws/${cepInformado}/json/`)
     .then(resultado => resultado.json())
     .then(json => {
-        if(json.erro){
-            mostrarTelaErro();
-        }else{
-            preencherCamposComJson();
-        }    
-    })
+        document.getElementById('cidade').value = json.localidade;
+        document.getElementById('uf').value = json.uf;
+    });
        
 }
 
-// function limparCampos() {
-//     document.getElementById("formulario__texto").innerHTML = "";
-//     // document.getElementById("corpoTabela").innerHTML = ""; 
-// }
+ function limparCampos() {
+    nome.value = '';
+    cnpj.value = '';
+    cep.value = '';
+    cidade.value = '';
+    uf.value = '';
+ }
 
-function preencherCamposComJson(json) {
-    // ESSA condição funciona em javascript, é o mesmo que
-    //if(json.bairro != undefinde && json.bairro != '')
-    if (json.localidade) {
-        //Obter o componente pelo ID , funciona!! 
-        cidade.value = json.localidade;
-        if (json.uf) {
-            uf.value = json.uf;
-        }
-    } else {
-        alert("Erro ao preencher os campos cidade e UF");
-    }
-} 
+
+// function preencherCamposComJson(json) {
+//     // ESSA condição funciona em javascript, é o mesmo que
+//     //if(json.bairro != undefinde && json.bairro != '')
+//     if (json.localidade) {
+//         //Obter o componente pelo ID , funciona!! 
+//         cidade.value = json.localidade;
+//         if (json.uf) {
+//             uf.value = json.uf;
+//         }
+//     } else {
+//         alert("Erro ao preencher os campos cidade e UF");
+//     }
+// } 
 
 
 function mostrarTelaErro() {
