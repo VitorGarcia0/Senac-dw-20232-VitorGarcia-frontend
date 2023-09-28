1f3a4592e9b2528ce869a8f0ea35a0ad0f52753d
Vitor Garcia de Vasconcelos <vitorgarciadevasconcelos@gmail.com>
2023-09-28 11:08:16 -0300

    Fazendo o routing da Listagem de Produtos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { ProdutosModule } from './produtos/produtos.module';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "produtos/lista", pathMatch: "full"},
+  {
+    path: 'produtos', //Isso é o que vai estar na URL
+    loadChildren:() => import('./produtos/produtos.module').then(m => m.ProdutosModule)
+    // import, e o caminho até chegar na rota
+    }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
